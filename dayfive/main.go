eaca7c11dbcf3190c7a1ff848343b2c39aca2e92
wulihong <wlh@yctc.tech>
2022-02-09 09:57:22 +0800

    添加注释

--- dayfive/main.go
+++ dayfive/main.go
@@ -1,34 +1,32 @@
 package main
 
-import "fmt"
-
 // 下面这段代码能否通过编译？不能的话，原因是什么？如果通过，输出什么？
 func main() {
-	sn1 := struct {
-		age  int
-		name string
-	}{age: 11, name: "qq"}
-	sn2 := struct {
-		name string
-		age  int
-	}{age: 11, name: "qq"}
-
-	if sn1 == sn2 {
-		fmt.Println("sn1 == sn2")
-	}
-
-	sm1 := struct {
-		age int
-		m   map[string]string
-	}{age: 11, m: map[string]string{"a": "1"}}
-	sm2 := struct {
-		age int
-		m   map[string]string
-	}{age: 11, m: map[string]string{"a": "1"}}
-
-	if sm1 == sm2 {
-		fmt.Println("sm1 == sm2")
-	}
+	// sn1 := struct {
+	// 	age  int
+	// 	name string
+	// }{age: 11, name: "qq"}
+	// sn2 := struct {
+	// 	name string
+	// 	age  int
+	// }{age: 11, name: "qq"}
+	//
+	// // if sn1 == sn2 {
+	// // 	fmt.Println("sn1 == sn2")
+	// // }
+	//
+	// sm1 := struct {
+	// 	age int
+	// 	m   map[string]string
+	// }{age: 11, m: map[string]string{"a": "1"}}
+	// sm2 := struct {
+	// 	age int
+	// 	m   map[string]string
+	// }{age: 11, m: map[string]string{"a": "1"}}
+	//
+	// if sm1 == sm2 {
+	// 	fmt.Println("sm1 == sm2")
+	// }
 
 	/*
 		不能编译通过   invalid operation: sm1 == sm2
@@ -39,6 +37,5 @@ func main() {
 		4、bool、数值类型、字符、指针、数组等是可以比较的
 		5、切片、map、函数等是不能比较的。
 		6、参考文档 https://golang.org/ref/spec#Comparison_operators
-
 	*/
 }
